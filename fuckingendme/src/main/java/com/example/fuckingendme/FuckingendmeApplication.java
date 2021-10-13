7f7b38529a66d80febb5776039fc075957bf2fbe
MrBigRedButton <73118706+MrBigRedButton@users.noreply.github.com>
2021-10-13 23:15:29 +0200

    pleasse

---
+++ fuckingendme/src/main/java/com/example/fuckingendme/FuckingendmeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuckingendme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuckingendmeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FuckingendmeApplication.class, args);
+    }
+
+}
---
+++ fuckingendme/src/test/java/com/example/fuckingendme/FuckingendmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fuckingendme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuckingendmeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
